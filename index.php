60acddb421e3147a760d7f5e4770c16410546a8b
Natan Felles <natanfelles@gmail.com>
2017-07-08 21:35:25 -0300

    Add params in AdminerISPConfig constructor

--- index.php
+++ index.php
@@ -4,21 +4,20 @@ ini_set('display_errors', 0);
 
 function adminer_object()
 {
-	// Required to run any plugin.
-	include_once "./plugins/plugin.php";
+    // Required to run any plugin.
+    include_once "./plugins/plugin.php";
 
-	// Plugins auto-loader.
-	foreach (glob("plugins/*.php") as $filename)
-	{
-		include_once "./$filename";
-	}
+    // Plugins auto-loader.
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
 
-	// Specify enabled plugins here.
-	$plugins = array(
-		new AdminerISPConfig(),
-	);
+    // Specify enabled plugins here.
+    $plugins = array(
+        new AdminerISPConfig('admin', 'admin', 'https://192.168.10.100:8080/remote/', false),
+    );
 
-	return new AdminerPlugin($plugins);
+    return new AdminerPlugin($plugins);
 }
 
 // Include original Adminer or Adminer Editor.
